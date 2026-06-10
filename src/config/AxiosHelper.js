a48bb205d4cb22d6a231f4ca662d90a615d2b90d
Abhinavkr004 <abhinav.kumar1272004@gmail.com>
2026-06-10 19:36:17 +0530

    fixed url

--- src/config/AxiosHelper.js
+++ src/config/AxiosHelper.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const baseURL= 'https://chatapp-backend-2-n84o.onrender.com';
+export const baseURL= 'https://chatapp-backend-1-f1wn.onrender.com';
 export const httpClient = axios.create({
     baseeURL: baseURL,
 });
