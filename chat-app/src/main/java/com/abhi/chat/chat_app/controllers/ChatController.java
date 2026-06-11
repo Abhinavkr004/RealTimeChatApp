e5491f9137910724c9916769783afecd277e5d3e
Abhinav kumar <abhinav.kumar1272004@gmail.com>
2026-06-11 21:36:13 +0530

    Update ChatController.java

--- chat-app/src/main/java/com/abhi/chat/chat_app/controllers/ChatController.java
+++ chat-app/src/main/java/com/abhi/chat/chat_app/controllers/ChatController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.time.LocalDateTime;
 
 @Controller
-@CrossOrigin(origins = {"https://real-time-chat-app-nine-tau.vercel.app"})
+@CrossOrigin("http://localhost:5173")
 public class ChatController {
     // it is basically for handle the chats
     private RoomRepository roomRepository;
